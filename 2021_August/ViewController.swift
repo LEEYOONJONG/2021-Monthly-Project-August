10ff96727f644d208e97840113d8f26e6a2e1346
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-20 18:57:25 +0900

    Initial Commit

---
+++ 2021_August/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2021_August
+//
+//  Created by YOONJONG on 2021/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
