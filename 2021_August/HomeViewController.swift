af05a195867d5bc011c493c96ce7d218789c9425
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-27 18:36:14 +0900

    Add night mode color options, button animation

--- 2021_August/HomeViewController.swift
+++ 2021_August/HomeViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 import KeychainSwift
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var todayCommitView: UIView!
+    @IBOutlet weak var continuousCommitView: UIView!
+    
+    @IBOutlet weak var todayCommitButton: UIButton!
     @IBOutlet weak var commitNumLabel: UILabel!
     @IBOutlet weak var continuousCommitNumLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         OperationQueue().addOperation {
             if KeychainSwift().get("accessToken") == nil {
                 LoginManager.shared.requestCode()
@@ -34,6 +39,10 @@ class HomeViewController: UIViewController {
         }
     }
     
+    // dark mode 여부 바뀌면 색상 즉시 변경
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        updateUI()
+    }
     @IBAction func githubSignupClicked(_ sender: Any) {
         if KeychainSwift().get("accessToken") == nil {
             LoginManager.shared.requestCode()
@@ -58,6 +67,13 @@ class HomeViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     @IBAction func todayCommitClicked(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.todayCommitButton.transform = CGAffineTransform(rotationAngle:(180.0 * .pi)/180);
+        })
+        UIView.animate(withDuration: 0.3, animations: {
+            self.todayCommitButton.transform = CGAffineTransform(rotationAngle:(360.0 * .pi)/180);
+        })
+        
         OperationQueue().addOperation {
             LoginManager.shared.getUser()
             
@@ -71,15 +87,34 @@ class HomeViewController: UIViewController {
         }
     }
     
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    func updateUI(){
+        // 버전별, 다크모드 여부에 따라 색상 따로 설정
+        let todayCommitViewColor = UIColor {(trait) -> UIColor in
+            if #available(iOS 13, *){
+                if trait.userInterfaceStyle == .dark {
+                    return #colorLiteral(red: 0.1294117719, green: 0.2156862766, blue: 0.06666667014, alpha: 1)
+                } else {
+                    return #colorLiteral(red: 0.9493537545, green: 0.9761642814, blue: 0.9253637195, alpha: 1)
+                }
+            } else {
+                return #colorLiteral(red: 0.9493537545, green: 0.9761642814, blue: 0.9253637195, alpha: 1)
+            }
+        }
+        let continuousCommitViewColor = UIColor {(trait) -> UIColor in
+            if #available(iOS 13, *){
+                if trait.userInterfaceStyle == .dark {
+                    return #colorLiteral(red: 0.07951391488, green: 0.1685524583, blue: 0.3377684057, alpha: 1)
+                } else {
+                    return #colorLiteral(red: 0.890386641, green: 0.9659765363, blue: 0.9694517255, alpha: 1)
+                }
+            } else {
+                return #colorLiteral(red: 0.890386641, green: 0.9659765363, blue: 0.9694517255, alpha: 1)
+            }
+        }
+        todayCommitView.layer.cornerRadius = 10
+        continuousCommitView.layer.cornerRadius = 10
+        todayCommitView.layer.backgroundColor = todayCommitViewColor.cgColor
+        continuousCommitView.layer.backgroundColor = continuousCommitViewColor.cgColor
+    }
     
 }
